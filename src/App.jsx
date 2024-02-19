ba76a7ac8f665a3edc425e7946143c71840e54bf
k1z1n <martynm1314@gmail.com>
2024-02-19 16:05:40 +0300

    fix new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from "./pages/Home/HomePage.jsx";
-import CartPage from "./pages/Cart/CartPage.jsx";
 import ProductPage from "./pages/Product/ProductPage.jsx";
 import CatalogPage from "./pages/Catalog/CatalogPage.jsx";
 import './index.css';
